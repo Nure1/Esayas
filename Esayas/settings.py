6290c0c664280f04b5e838ecef78ba1c48a94bc0
Nure <ibrahimsaladin1@gmail.com>
2023-06-16 11:54:05 +0300

    postgres database

--- Esayas/settings.py
+++ Esayas/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'Esayas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'HOST': 'containers-us-west-199.railway.app',
+        'PORT': '5621',
+        'USER': 'postgres',
+        'PASSWORD': 'vL4Uuab2ERgrbxmaParT',
     }
 }
 
